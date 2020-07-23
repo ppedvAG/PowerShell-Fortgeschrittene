e89961fe44242f32da407b1ab7d19a72bfa1ba12
Stefan Ober <StefanO@ppedv.de>
2020-07-23 17:09:27 +0200

    Lösung für try Catch übung hochgeladen

---
+++ TryCatchÜbungsLösung.ps1
@@ -0,0 +1,14 @@
+﻿[cmdletBinding()]
+param(
+    [string]$Path)
+
+try
+{
+(Get-ChildItem -Path $Path -File -ErrorAction Stop  | Measure-Object).Count
+}
+catch
+{
+    Write-Verbose -Message $Error[0]
+    Write-Host "Es ist ein Fehler aufgetreten" -ForegroundColor Red
+    
+}
