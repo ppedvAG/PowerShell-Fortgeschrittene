95c7ac1a8ba3287cbdec458b164a61cf34eaa781
Stefan Ober <StefanO@ppedv.de>
2020-07-23 15:52:46 +0200

    Ergebnisskripte aus Übung zum Skripte zum erstellen eines TestFiles Ordner

---
+++ TestFilesSkripte/MichaelF.ps1
@@ -0,0 +1,66 @@
+﻿<#
+.Synopsis
+   Erstellt einen Ordner "TestFiles" gefüllt mit beliebig vielen Ordnern und 
+   füllt diese mit beliebig vielen Txt dateien, solange beliebig <=10
+
+.EXAMPLE
+skirpt1.ps1 -path C:\Testfiles\ -dircount 1 -filecount 0
+Erstellt unter dem Pfad TestFiles einen ORdner TestFiles mit einem Ordner und Null Dateien
+Ausgabe:
+Verzeichnis: C:\Testfiles
+
+
+Mode                LastWriteTime         Length Name
+----                -------------         ------ ----
+d-----       23.07.2020     12:02                TestFile
+
+
+    Verzeichnis: C:\Testfiles\TestFiles
+
+
+Mode                LastWriteTime         Length Name
+----                -------------         ------ ----
+d-----       23.07.2020     12:02                Ordner1
+#>
+
+[cmdletBinding()]
+
+Param(
+[Parameter(Mandatory=$true)]
+[ValidateScript({Test-Path -Path $PSItem -isValid})]
+[string] $path,
+
+[ValidateRange(0,10)]
+[int] $dircount = 3,
+
+[ValidateRange(0,10)]
+[int] $filecount = 3
+)
+
+Write-Debug -Message "Vor Pfaderstellung"
+$path = $path + "\TestFiles"
+Write-Debug -Message "Nach Pfaderstellung"
+if(Test-Path -Path $path)
+{
+    Remove-Item -Path $path -Force -Recurse
+}
+
+New-Item -Path $path -ItemType Directory
+
+for($i = 0; $i -lt $dircount; $i++)
+{
+    New-Item -Path $path -Name "\Ordner$i" -ItemType Directory 
+        for($j = 0; $j -ne $filecount; $j++)
+        {
+            $tempi = $path + "\Ordner$i\Datei$j.txt"
+            New-Item -Path $tempi -ItemType File 
+        }
+}
+
+
+for($j = 0; $j -ne $filecount; $j++)
+{         
+      New-Item -Path $path -Name "Datei$j.txt" -ItemType File 
+}
+
+
---
+++ TestFilesSkripte/StefanA.ps1
@@ -0,0 +1,48 @@
+﻿[cmdletBinding()] #Importiert erweiterte Parameterfunktionen 
+Param(
+[Parameter (Mandatory=$true)]
+[ValidateScript({Test-Path -IsValid -Path $PSItem})] #Der Parameter -isValid prüft ob die Syntax des PFades richtig ist, ohne das die Bestandteile des PFades vorhanden sein müssen
+[string]$TestFileFolderpath,
+
+[ValidateRange(1,10)]
+[int]$SubFolderCount = 1,
+
+[ValidateRange(1,10)]
+[int]$FileCount = 1,
+
+[switch]$force
+)
+
+if($force -and (Test-Path -Path $TestFileFolderpath))
+{
+    Remove-Item -Path $TestFileFolderpath -Force -Recurse
+}
+elseif(Test-Path -Path $TestFileFolderpath)
+{
+    Write-Host "Der Pfad ist schon vorhanden"
+    break
+}
+
+
+
+Write-Verbose -Message "Vor Variablenwerte Ausführung:TestFileFolder: $TestFileFolder, SubFolderCount: $SubFolderCount, FileCount: $FileCount"
+# Hauptordner erstellen
+New-Item -Path $TestFileFolderpath -ItemType "directory"
+# Files erstellen im Hauptordner
+for ($i = 1; $i -le $FileCount; $i=$i+1) 
+{
+    $filenumber = "{0:D2}" -f $i #Decimale Formatierung um führende Nullen zu erhalten 
+    New-Item -Path "$TestFileFolderpath\" -Name "File$filenumber.txt" -ItemType "file" -Value "Testinhalt" 
+}
+
+
+# Unterordner erstellen
+for ($i = 1; $i -le $SubFolderCount; $i=$i+1) 
+{ 
+    New-Item -Path $TestFileFolderpath -Name "Ordner$i" -ItemType "directory"
+    for ($f = 1; $f -le $FileCount; $f=$f+1) 
+    { 
+        New-Item -Path "$TestFileFolderpath\Ordner$i\" -Name "File$f.txt" -ItemType "file" -Value "Testinhalt"
+    }      
+}
+ 
