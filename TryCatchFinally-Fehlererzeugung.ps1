88114bb1a0e199580da1f88007586b153d8b7093
Stefan Ober <StefanO@ppedv.de>
2020-07-23 16:48:45 +0200

    Beispiel für Try Catch Finally hinzugefügt und Erzeugung manueller Fehler

---
+++ TryCatchFinally-Fehlererzeugung.ps1
@@ -0,0 +1,17 @@
+﻿try
+{
+    if((Test-Path -Path C:\fantasie -ErrorAction Stop) -ne $true) #Test-PAth liefert nur True oder False deswegen muss Fehler manuell ausgelöst werden
+    {
+       #nicht terminnierender Fehler Write-Error -Message "Pfad nicht vorhanden"
+       throw "Pfad nicht vorhanden" #Terminierender Fehler
+    }
+}
+catch
+{
+    
+    Write-Host "Pfad nicht verfügbar" -ForegroundColor Red
+}
+finally
+{
+    Write-Host "Ich werde immer ausgeführt" -ForegroundColor Magenta
+}
