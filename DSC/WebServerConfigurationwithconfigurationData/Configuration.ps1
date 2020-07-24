82764d0e4af9337e77382c4a83104ea235c87908
Stefan Ober <StefanO@ppedv.de>
2020-07-24 10:51:45 +0200

    DSC Configuration mit gesonderter KonfigurationData

---
+++ DSC/WebServerConfigurationwithconfigurationData/Configuration.ps1
@@ -0,0 +1,19 @@
+﻿configuration WebServerConfiguration
+{
+    # Zum Abrufen der Knotenliste können Ausdrücke ausgewertet werden.
+    # Beispiel: $AllNodes.Where("Role -eq Web").NodeName
+    node $AllNodes.NodeName
+    {
+        ForEach($Feature in $Node.WindowsFeature)
+        {
+            WindowsFeature $Feature.Name
+            {
+                Name = $Feature.Name
+                Ensure = $Feature.Ensure
+            }
+        }          
+    }
+}
+
+#Erzeugen der MOF Files durch ausführen der configuration. Wichtig hier muss dann eine ConfigurationData Datei mit der passenden Konfiguration angegeben werden
+#WebServerConfiguration -OutputPath .\WebServerConfigurationwithconfigurationData -ConfigurationData .\WebServerConfigurationwithconfigurationData\ConfigurationData.psd1
