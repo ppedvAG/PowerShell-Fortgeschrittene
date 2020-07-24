5d3566b8fe4ae31e085d5e806ac4c08a3675fc51
Stefan Ober <StefanO@ppedv.de>
2020-07-24 12:02:09 +0200

    Einfache Lösung zur Übung

---
+++ DSC/Uebung-Loesung/AspWebServerConfig.ps1
@@ -0,0 +1,27 @@
+﻿configuration AspWebServer
+{
+    # Zum Abrufen der Knotenliste können Ausdrücke ausgewertet werden.
+    # Beispiel: $AllNodes.Where("Role -eq Web").NodeName
+    node ("Member1")
+    {
+        # Ressourcenanbieter aufrufen
+        # Beispiel: WindowsFeature, File
+        WindowsFeature WebServer
+        {
+           Ensure = "Present"
+           Name   = "web-server"
+        }
+        WindowsFeature Asp
+        {
+           Ensure = "Present"
+           Name   = "Web-Asp-Net45"
+           DependsOn = "[WindowsFeature]WebServer"
+        }
+
+        Group IIS-Users
+        {
+            GroupName = "IIS-Users"
+            Ensure = "Present"
+        }      
+    }
+}
