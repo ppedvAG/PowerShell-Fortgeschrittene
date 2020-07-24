5b27c9fddc23fb78f2bdb91012522d31e6e7105e
Stefan Ober <StefanO@ppedv.de>
2020-07-24 11:07:44 +0200

    DSC Configuration mit ausgelagerter ConfigurationData und Konfiguration des LCM

---
+++ DSC/WebServerConfigurationwithconfigurationDataAndLCMConfig/Configuration.ps1
@@ -0,0 +1,26 @@
+﻿configuration WebServerConfiguration
+{
+    # Zum Abrufen der Knotenliste können Ausdrücke ausgewertet werden.
+    # Beispiel: $AllNodes.Where("Role -eq Web").NodeName
+    node $AllNodes.NodeName
+    {
+        #Konfigurieren des LCM (Local Configuration Manager)
+        LocalConfigurationManager
+        {
+            ConfigurationMode = "ApplyAndAutoCorrect"
+            ConfigurationModeFrequencyMins = 15 #standardwert Wertbereich in Minuten 15 bis 44640
+        }
+
+        ForEach($Feature in $Node.WindowsFeature)
+        {
+            WindowsFeature $Feature.Name
+            {
+                Name = $Feature.Name
+                Ensure = $Feature.Ensure
+            }
+        }          
+    }
+}
+
+#Erzeugen der MOF Files durch ausführen der configuration. Wichtig hier muss dann eine ConfigurationData Datei mit der passenden Konfiguration angegeben werden
+WebServerConfiguration -OutputPath C:\Kursbeispiele\DSC\WebServerConfigurationwithconfigurationDataAndLCMConfig -ConfigurationData C:\Kursbeispiele\DSC\WebServerConfigurationwithconfigurationDataAndLCMConfig\ConfigurationData.psd1
---
+++ DSC/WebServerConfigurationwithconfigurationDataAndLCMConfig/ConfigurationData.psd1
@@ -0,0 +1,18 @@
+﻿@{
+    AllNodes = @(
+        @{
+            NodeName = "Member1"
+            WindowsFeature = @(
+                @{
+                    Name = "Web-Server"
+                    Ensure = "Present"
+                },
+                @{
+                    Name = "Web-Mgmt-Tools"
+                    Ensure = "Present"
+                }
+            )
+          }         
+    )
+}
+# Speichert ConfigurationData in einer Datei mit der Dateierweiterung "PSD1".
